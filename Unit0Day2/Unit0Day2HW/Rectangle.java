92bd208cd857274b0c88cccb46fd89fce93100a8
victorli999 <victorli1076@gmail.com>
2022-08-30 19:05:35 -0700

    homework

---
+++ Unit0Day2/Unit0Day2HW/Rectangle.java
@@ -0,0 +1,39 @@
+package Unit0Day2HW;
+
+public class Rectangle {
+    private int length;
+    private int width;
+
+    public Rectangle(int newLength, int newWidth) {
+        length = newLength;
+        width = newWidth;
+    }
+
+
+    //methods
+    public int getPerimeter() {
+        return width * 2 + length * 2;
+    }
+
+    public int getArea() {
+        return width * length;
+    }
+
+    //setters
+    public void setLength(int newLength) {
+        length = newLength;
+    }
+
+    public void setWidth(int newWidth) {
+        width = newWidth;
+    }
+
+    //getters
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
