2e30a4163d4eb37c746ae50e59e2752ff6ed7bb3
victorli999 <victorli1076@gmail.com>
2022-11-21 12:37:20 -0800

    Unit2Lab3

--- Unit2/Lab3/Robot.java
+++ Unit2/Lab3/Robot.java
@@ -5,6 +5,12 @@ public class Robot {
     private int pos; // current position (tile number) of Robot
     private boolean facingRight; // true means this Robot is facing right
 
+    public Robot(int[] hall, int pos) {
+        this.hall = hall;
+        this.pos = pos;
+        facingRight = true;
+    }
+
     // postcondition: returns true if this robot has a wall immediately in
     // front of it, so it cannot move forward;
     // otherwise, returns false
@@ -20,10 +26,15 @@ public class Robot {
     // specifications above and the state of this
     // Robot has been updated
     private void move() {
-        if (facingRight) {
-            pos++;
-        } else {
-            pos--;
+        if (hall[pos] != 0) {
+            hall[pos]--;
+        }
+        if (hall[pos] == 0) {
+            if (forwardMoveBlocked() == false) {
+                pos += facingRight ? 1 : -1;
+            } else {
+                facingRight = !facingRight;
+            }
         }
     }
 
@@ -31,16 +42,10 @@ public class Robot {
     // returns the number of moves made
     public int clearHall() {
         int moves = 0;
-        while (hall[pos] > 0) {
-            hall[pos]--;
+        while (!hallIsClear()) {
+            move();
             moves++;
-            if (hall[pos] == 0) {
-                if (forwardMoveBlocked()) {
-                    facingRight = !facingRight;
-                } else {
-                    move();
-                }
-            }
+            displayState();
         }
         return moves;
     }
@@ -49,23 +54,27 @@ public class Robot {
     // otherwise, returns false
     private boolean hallIsClear() {
         int count = 0;
-        for (int k = 0; k < hall.length; k++)
+        for (int k = 0; k < hall.length; k++) {
             count += hall[k];
+        }
         return count == 0;
 
     }
 
     private void displayState() {
-        for (int k = 0; k < hall.length; k++)
+        for (int k = 0; k < hall.length; k++) {
             System.out.print(" " + hall[k]);
+        }
         System.out.println();
 
-        for (int k = 0; k < pos; k++)
+        for (int k = 0; k < pos; k++) {
             System.out.print("  ");
+        }
 
-        if (facingRight)
+        if (facingRight) {
             System.out.println(" >");
-        else
+        } else {
             System.out.println(" <");
+        }
     }
 }
