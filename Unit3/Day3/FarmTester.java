5da90762db054bd05ad641d1832511cf6aeb7194
victorli999 <victorli1076@gmail.com>
2022-12-11 21:26:09 -0800

    test prep

--- Unit3/Day3/FarmTester.java
+++ Unit3/Day3/FarmTester.java
@@ -3,25 +3,32 @@ package Unit3.Day3;
 public class FarmTester {
     public static void main(String[] args) {
 
-        Cat cat = new Cat("Rancho Cucamonga");
-        Cow cow = new Cow("Betsy");
-        Pig pig = new Pig("Babe");
+        // Cat cat = new Cat("Rancho Cucamonga");
+        // Cow cow = new Cow("Betsy");
+        // Pig pig = new Pig("Babe");
 
-        Food food = new Food("Grass");
-        cat.makeSound();
-        cat.potty();
-        cat.sleep();
+        // Food food = new Food("Grass");
+        // cat.makeSound();
+        // cat.potty();
+        // cat.sleep();
 
-        cow.makeSound();
-        cow.potty();
-        cow.sleep();
+        // cow.makeSound();
+        // cow.potty();
+        // cow.sleep();
 
-        pig.makeSound();
-        pig.potty();
-        pig.sleep();
+        // pig.makeSound();
+        // pig.potty();
+        // pig.sleep();
 
-        cat.eat(cow);
-        cow.eat(food);
-        // pig.eat(food);
+        // cat.eat(cow);
+        // cow.eat(food);
+        // // pig.eat(food);
+
+        // Animal[] a = { new Cat("GOD DAMN"), new Cow("Betsy"), new Pig("Babe") };
+        // System.out.println(((Cat) a[0]).getName());
+
+        Animal[] cat = { new Cat("a"), new Cat("b"), new Cat("c") };
+        for (Animal c : cat)
+            System.out.println(((Cat) c).getName());
     }
 }
